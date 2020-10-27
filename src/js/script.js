fbe31fe2cf7624c57a62818ce2947075afb86776
Irina <irinamiheeva13@gmail.com>
2020-10-27 15:32:20 +0300

    lesson 60

--- src/js/script.js
+++ src/js/script.js
@@ -50,3 +50,35 @@ $('.button_mini').each(function(i) {
       $('.overlay, #order').fadeIn('slow');
   });
 });
+
+
+function validateForms(form) {
+  $(form).validate({
+    rules: {
+      name: {
+        required: true,
+        minlength: 2
+      },
+      phone: "required",
+      email: {
+        required: true,
+        email: true,
+      }
+    },
+    messages: {
+      name: {
+        required: "Пожалуйста, введите свое имя",
+        minlength: jQuery.validator.format('Введите {0} символа')
+      },
+      phone: "Пожалуйста, введите свой номер телефона",
+      email: {
+        required: "Пожалуйста, введите свой почтовый адрес",
+        email: "Неправильный формат адреса почты"
+      }
+    }
+  });
+};
+
+validateForms('#consultation-form');
+validateForms('#consultation form');
+validateForms('#order form');
